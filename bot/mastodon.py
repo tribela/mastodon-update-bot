a957becc06c932a813aa5414d80a31e5360a9e4b
kjwon15 <kjwonmail@gmail.com>
2019-06-10 02:15:58 +0900

    Fix mention correctly

--- bot/mastodon.py
+++ bot/mastodon.py
@@ -99,7 +99,7 @@ class MastodonStreamListener(mastodon.StreamListener):
 
         if update_type not in valid_values:
             self.post(
-                f'Invalid type. valid types are {", ".join(valid_values)}',
+                f'@{acct} Invalid type. valid types are {", ".join(valid_values)}',
                 visibility='direct', in_reply_to_id=reply_id)
 
         self.logger.info(f'Changing update type of {acct} to {update_type}')
@@ -109,13 +109,13 @@ class MastodonStreamListener(mastodon.StreamListener):
 
         if not admin:
             self.post(
-                'You are not registered. Please send me "register" to register you.',
+                '@{acct} You are not registered. Please send me "register" to register you.',
                 visibility='direct', in_reply_to_id=reply_id)
         else:
             admin.update_type = UpdateType(update_type)
             session.commit()
             self.post(
-                f'Changed update type to {update_type}',
+                f'@{acct} Changed update type to {update_type}',
                 visibility='direct', in_reply_to_id=reply_id)
 
         session.close()
